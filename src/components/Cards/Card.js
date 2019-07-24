d3d213baa07264349e0db7ca6ecbc7609494af75
kshriva1 <kundan0395@gmail.com>
2019-07-24 18:34:57 -0400

    added moment to convert unix time to proper format

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -1,4 +1,5 @@
 import React,{Fragment} from 'react';
+import Moment from 'react-moment';
 
 const Card = ({ creationDate, firstName, middleName, lastName }) => {
   if(!firstName){
@@ -15,8 +16,8 @@ const Card = ({ creationDate, firstName, middleName, lastName }) => {
     <span>
     <div className='tc grow bg-light-yellow br3 pa5 ma3 dib bw2 shadow-5'>
       <div>
-        <h2>{creationDate}</h2>
-        <p>{firstName + ' ' + middleName + ' ' + lastName}</p>
+          <h2><Moment date={creationDate} /></h2>
+        <h5><p>{firstName + ' ' + middleName + ' ' + lastName}</p></h5>
       </div>
     </div>
     </span>
